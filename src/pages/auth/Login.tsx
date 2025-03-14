d1592f9f01058a56ee0055bad989bf4cf8e117bb
Mark Louis Ayaay Bernardo <marklouisbernando318@gmail.com>
2025-03-14 16:38:19 +0800

    Added Login UI (#1)

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Button } from '@/components/ui/button'
-
 
 export const Login:React.FC = () => {
   return (
-    <div>
-        <Button variant="default">Login</Button>
-    </div>
-    
+    <main className='w-full h-screen flex justify-center items-center'>
+      <div>
+        <h2 className='font-bold text-brand-primary text-2xl'>Resibo Pilipinas</h2>
+        <p className='text-center font-secondary text-sm'>Electronic Invoicing</p>
+      </div>
+      <div className='mt-5'>
+        
+      </div>
+    </main>
   )
 }
