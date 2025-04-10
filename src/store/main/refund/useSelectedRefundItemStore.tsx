8170f4e36ca1988af56374df49f5f2d1c9be555d
MarkmeUbuntu <marklouisbernando318@gmail.com>
2025-04-10 16:07:33 +0800

    feat: added compute vat computation

--- src/store/main/refund/useSelectedRefundItemStore.tsx
+++ src/store/main/refund/useSelectedRefundItemStore.tsx
@@ -6,16 +6,34 @@ type SingleRefundItemType = refundStoreType['items'][number];
 type selectedRefundItemType = {
     selectedItem: SingleRefundItemType;
     setSelectedItem: (item: SingleRefundItemType) => void;
+    selectedItemsComputations : {
+        vatable_amount: number,
+        vat_output_tax: number,
+        total_amount: number
+    }
     setDecrementQuantityItem: (id: number) => void;
+}
+
+
+const computeVAT = (item_bought: SingleRefundItemType['item_bought']) => {
+    const vatable_amount = item_bought?.reduce((acc, curr) => acc + (curr.total_cost * curr.quantity), 0) || 0;
+    const vat_output_tax = parseFloat((vatable_amount * 0.12).toFixed(2));
+    const total_amount = parseFloat((vatable_amount + vat_output_tax).toFixed(2));
+
+    return { vatable_amount, vat_output_tax, total_amount };
 }
 
 export const useSelectedRefundItemStore = create<selectedRefundItemType>((set) => ({
     selectedItem: {} as SingleRefundItemType,
-    setSelectedItem: (item) => set({ selectedItem: item }),
+    selectedItemsComputations: {
+        vatable_amount: 0,
+        vat_output_tax: 0,
+        total_amount: 0,
+    },
     setDecrementQuantityItem: (id) => {
-
+        
         set((state) => {
- 
+            
             const updatedItems = state.selectedItem.item_bought?.map((item) => {
                 if (item.id === id) {
                     return {
@@ -25,14 +43,25 @@ export const useSelectedRefundItemStore = create<selectedRefundItemType>((set) =
                 }
                 return item;
             });
-            
-            return {
-                selectedItem: {
-                    ...state.selectedItem,
-                    item_bought: updatedItems,
-                },
-            }
-
+            const updatedSelectedItem = {
+                ...state.selectedItem,
+                item_bought: updatedItems,
+            };
+        
+            const computations = computeVAT(updatedItems);
+        
+              return {
+                selectedItem: updatedSelectedItem,
+                selectedItemsComputations: computations,
+              };
         });
-    }
+    },
+    setSelectedItem: (item) => {
+        const computations = computeVAT(item.item_bought || []);
+        set({
+          selectedItem: item,
+          selectedItemsComputations: computations,
+        });
+      },
+    
 }))
